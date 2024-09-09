7ee9f7940d9cd137f2a55c7b87cf41e0d819ba60
Julia <veluat@gmail.com>
2024-09-09 20:55:14 +0300

    refactor: add ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
-
 import { createRoot } from 'react-dom/client'
-import App from './App'
-
+import App from './app/App.tsx'
+import { ThemeProvider } from '@/context/ThemeContext.tsx'
 import './style/index.scss'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 )
